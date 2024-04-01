e6d1ee88b48f3166aa026348a94c4efe3b5fab58
Lakshman Gupta <guptalakshman96.lg@gmail.com>
2024-04-01 18:31:21 +0530

    deviding compoents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { EcommerceComponent } from './components/ecommerce/ecommerce.component';
 
 export const routes: Routes = [
     { path: 'ecommerce', component: EcommerceComponent },
 ];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
